00fd30cb3066544053b43d3efdf8f34a5f86c727
GuerrillaGorilla <Guerrilla.Gorilla@outlook.com>
2023-10-16 12:54:59 -0400

    Updated lcuState

--- vite.config.ts
+++ vite.config.ts
@@ -29,47 +29,6 @@ export default ({ mode }) => {
           });
         },
       },
-      '/api/habistack': {
-        target: 'https://fathym-cloud-prd.azure-api.net/habistack/weather/ground/',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api\/habistack/, ''),
-        configure: (proxy, _options) => {
-          proxy.on('proxyReq', function(proxyReq, req, res, options) {
-            proxyReq.setHeader('lcu-subscription-key', process.env.VITE_HABISTACK_LCU_SUBSCRIPTION_ID);
-          });
-          proxy.on('error', (err, _req, _res) => {
-            console.log('proxy error', err);
-          });
-          proxy.on('proxyReq', (proxyReq, req, _res) => {
-            console.log('Sending Request to the Target:', req.method, req.url);
-          });
-          proxy.on('proxyRes', (proxyRes, req, _res) => {
-            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
-          });
-        },
-      },
-      '/api/geocodio': {
-        target: 'https://api.geocod.io/v1.7/',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api\/geocodio/, ''),
-        configure: (proxy, _options) => {
-          proxy.on('proxyReq', function(proxyReq, req, res, options) {
-            let connect = proxyReq.path.indexOf('?') > 0 ? '&' : '?';
-            proxyReq.path = `${proxyReq.path}${connect}api_key=${process.env.VITE_GEOCODIO_API_KEY}`;
-          });
-          proxy.on('error', (err, _req, _res) => {
-            console.log('proxy error', err);
-          });
-          proxy.on('proxyReq', (proxyReq, req, _res) => {
-            console.log('Sending Request to the Target:', req.method, req.url);
-          });
-          proxy.on('proxyRes', (proxyRes, req, _res) => {
-            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
-          });
-        },
-      }
     },
   },
 })
